a156dacc4ad6274b3859983a216e2b3cb9e2411d
Mohammad Amin Afzali <MA0030@edu.ornskoldsvik.se>
2020-12-01 21:08:44 +0100

    Första dagen klar

--- 2020/Day_1.py
+++ 2020/Day_1.py
@@ -8,14 +8,18 @@ answere = 0
 for item in range(len(data)):
     if not run:
         break
-    for secondItem in range(len(data)):
-        if (int(data[item]) + int(data[secondItem]) == 2020) & (item != secondItem):
-            array.append(data[item])
-            array.append(data[secondItem])
-            answere = int(data[item]) * int(data[secondItem])
-            run = False
+    for secondItem in range(item, len(data)):
         if not run:
             break
+        for thirdItem in range(secondItem, len(data)):
+            if int(data[item]) + int(data[secondItem]) + int(data[thirdItem]) == 2020:
+                array.append(int(data[item]))
+                array.append(int(data[secondItem]))
+                array.append(int(data[thirdItem]))
+                answere = int(data[item]) * int(data[secondItem]) * int(data[thirdItem])
+                run = False
+            if not run:
+                break
 print(array)
-print(1184 + 836)
+print(sum(array))
 print(answere)

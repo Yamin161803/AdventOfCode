a939f51455e2433d982c822ad0931450f472551b
Mohammad Amin Afzali <MA0030@edu.ornskoldsvik.se>
2020-12-02 07:55:54 +0100

    fösta tjärnan för andra dagen

---
+++ 2020/Day_2.py
@@ -0,0 +1,27 @@
+from os import spawnl
+
+
+with open("2020/Day_2.txt", "r") as item:
+
+    file = item.read().split("\n")
+data = []
+for item in range(len(file)):
+    data.append(file[item].split(":"))
+
+
+answer = 0
+
+for item in data:
+    temp = item[0].split(" ")
+    minNumber, maxNumber = map(int, temp[0].split("-"))
+    alfabetet = temp[1]
+    string = list(item[1])
+    count = 0
+    for char in range(1, len(string)):
+        if string[char] == alfabetet:
+            count += 1
+    if (count >= minNumber) & (count <= maxNumber):
+        print(item)
+        answer += 1
+
+print(answer)

fa82244ee36a122100ad49d90ad39694ddcf4ccd
Mohammad Amin Afzali <MA0030@edu.ornskoldsvik.se>
2020-12-04 08:27:07 +0100

    andra stjärnan dag 5

--- 2020/Day_4.py
+++ 2020/Day_4.py
@@ -1,23 +1,49 @@
 import re
-with open("Day_4.txt", "r") as item:
-    file = item.read().split("\n\n")
 
 
+def dayThree(fileName):
+    with open(fileName, "r") as item:
+        file = item.read().split("\n\n")
 
-answer=0
-for item  in file:
-    temp = []
-    correct = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    temp=(re.split('\n| ',item))
-    print(temp)
-    
-    for value in temp:
-        print(value[:3])
-        if value[:3] in correct:
-            correct.remove(value[:3])
+    answer = 0
+    for item in file:
+        color = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+        requirements = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+        temp = re.split("\n| ", item)
+        for secondItem in temp:
+            remove = False
+            array = secondItem.split(':')
+            if array[0] == "byr":
+                if (int(array[1]) <= 2002) & (int(array[1]) >= 1920):
+                    remove = True
+            if array[0] == "iyr":
+                if (int(array[1]) >= 2010) & (int(array[1]) <= 2020):
+                    remove = True
+            if array[0] == "eyr":
+                if (int(array[1]) >= 2020) & (int(array[1]) <= 2030):
+                    remove = True
+            if array[0] == "hgt":
+                if (array[1][-2:] == 'cm') or (array[1][-2:] == 'in'):
+                    if (array[1][-2:] == 'cm') & (int(array[1][:-2]) >= 150) & (int(array[1][:-2]) <= 193):
+                        remove = True
+                    if (array[1][-2:] == 'in') & (int(array[1][:-2]) >= 59) & (int(array[1][:-2]) <= 76):
+                        remove = True
+
+            if array[0] == "hcl":
+                if (array[1][:1] == "#") & (len(array[1]) == 7) & (
+                        not bool(re.compile(r'[^a-z0-9.]').search(array[1][1:]))):
+                    remove = True
+            if array[0] == "ecl":
+                if array[1] in color:
+                    remove = True
+            if array[0] == "pid":
+                if len(array[1]) == 9:
+                    remove = True
+            if remove:
+                requirements.remove(array[0])
+        if not requirements:
+            answer += 1
+    return answer
 
 
-    if not correct:
-        answer+=1
-
-print(answer)
+print(dayThree("Day_4.txt"))

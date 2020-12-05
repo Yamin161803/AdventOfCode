7679b9efcca0110dfec8abaefd054394be666237
Mohammad Amin Afzali <MA0030@edu.ornskoldsvik.se>
2020-12-05 09:09:48 +0100

    day 5 first star

---
+++ 2020/Day_5.py
@@ -0,0 +1,23 @@
+def dayFive(fileName):
+    with open(fileName, "r") as item:
+        data = item.read().split("\n")
+
+    answer=[]
+    for item in data:
+        availableSetsRemaining=128
+        seatNumber=127
+        columns=7
+        availableColumnsRemaining = 8
+        for value in range(len(item)):
+            if value <=6:
+                availableSetsRemaining /= 2
+                if item[value]=="F":
+                    seatNumber-=availableSetsRemaining
+            else:
+                availableColumnsRemaining/=2
+                if item[value]=="L":
+                    columns -= availableColumnsRemaining
+        answer.append(int(seatNumber*8 + columns))
+    return max(answer)
+
+print(dayFive("Day_5.txt"))
